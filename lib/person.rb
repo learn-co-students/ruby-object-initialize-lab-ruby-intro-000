b69c098505efda1c9e36bb13ded3f25607179ea0
Salvatore McCarty <salvatore.mccarty@gmail.com>
2016-12-22 18:42:59 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def initialize(name)
+    @name = name
+  end
+  def name                    #get name of Person
+    @name
+  end
+  def name=(name)             #set name of Person
+    @name = name
+  end
+  def job                     #get job of Person
+    @job
+  end
+  def job=(job)               #set job of Person
+    @job = job
+  end
+end
