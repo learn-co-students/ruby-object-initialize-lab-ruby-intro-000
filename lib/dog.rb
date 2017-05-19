32d49cad0e45f841c43dcf94fc18f2f1beaa8dc5
Haim Khaymovich <haim46@gmail.com>
2017-05-19 00:59:16 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+
+  def initialize(name, breed="Mutt")
+    @name = name
+    @breed =breed
+  end
+
+  def name=(name, breed)
+    @name = name
+    @breed =breed
+  end
+
+  def name(name,breed)
+    @name
+    @breed
+  end
+end
