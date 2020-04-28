e13da88b2e1f3c8d54d7ed942bbad9c2e7b66dfd
Miao Nan Lin <miaonanlin@gmail.com>
2020-04-28 15:52:12 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  def initialize(name,breed)
+    @name = name
+    @breed = "Mutt"
+  end
+
+  def name_breed=(name, breed)
+    @name = name
+    @breed = "Mutt"
+  end
+
+  def name_breed
+    @name
+    @breed
+  end
+
+
+end
