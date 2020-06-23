e680feecac1536d93c425739af547d2cfce598b9
Chandler Hanson <chan.hanson5@gmail.com>
2020-06-23 23:01:08 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+class Dog
+    def initialize(name, dog_breed = "Mutt")
+      @name = name
+      @breed = dog_breed
+    end
+    def name
+      @name
+    end
+    def breed
+      @breed
+    end
+
+  end
