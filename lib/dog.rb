aa36356e2a747f7ff767dfd4fec2a9a29c07e80a
JINOOK JUNG <jinook929@gmail.com>
2020-11-13 03:08:34 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog 
+  def initialize(name, breed="Mutt")
+    @name = name
+    @breed = breed 
+  end 
+  
+  def name 
+    @name = name 
+  end 
+  
+  def name=(this_name) 
+    @name = this_name 
+  end 
+  
+  def breed 
+    @breed = breed 
+  end 
+  
+  def breed=(this_breed) 
+    @breed = this_breed 
+  end 
+end 
