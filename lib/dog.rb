55c53f03e09ed9377e9674051eb7b5364a30217e
edward arias <jaysongraphics@gmail.com>
2021-04-01 16:04:22 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+        def initialize(name= "Fido", breed = "Mutt")
+         @name = name
+         @breed = breed
+        end
+                  def name= (name)
+                       @name = name
+                    end
+              def breed= (bread)
+                @breed = breed
+              end
+           def name
+             @name
+            end
+       def breed
+         @breed
+       end
+end
